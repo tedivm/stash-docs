a5d645cb15ff10ec96a1f5f2deabe4a77c932086
Robert Hafner <tedivm@tedivm.com>
2012-11-10 16:12:24 -0800

    Changed sidebar width
    Limited the sidebar widtch to 175px.

--- conf.py
+++ conf.py
@@ -100,9 +100,7 @@ html_theme = 'nature'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {}
-
-
+html_theme_options = {'sidebarwidth': 175}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
